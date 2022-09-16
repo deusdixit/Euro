2962c7ffb4cd5f14f6b31cd8592da74408053212
Niels Gasper <niels@gasper.id>
2022-09-16 13:23:03 +0200

    add assertJ and testcase why double is not good

--- src/main/java/de/materna/currency/Euro.java
+++ src/main/java/de/materna/currency/Euro.java
@@ -2,10 +2,16 @@ package de.materna.currency;
 
 import java.text.DecimalFormat;
 
-public class Euro {
+/**
+ * Euro is a class that describes the Euro Currency.
+ *
+ * This class is immutable and stores the value as double.
+ */
+public final class Euro {
 
     private final double wert;
     private final DecimalFormat df = new DecimalFormat( "###.## €" );
+
 
 
     private Euro(double value) {
@@ -15,21 +21,37 @@ public class Euro {
         this.wert = value;
     }
 
-    // Erzeugen von immutable Euro-Objekten durch Fabrikmethode
+    /** Erzeugen von immutable Euro-Objekten durch Fabrikmethode
+     *
+     * @param value - Value greater than or equal to 0
+     * @return - new Instance of Euro with the specified value
+     */
     public static Euro of(double value) {
         return new Euro(value);
     }
 
-    // erfragt den Wert
+    /**
+     * Gets the value as double
+     * @return - value as double
+     */
     public double value() {
         return this.wert;
     }
 
+    /**
+     * Get the Euro as a string in decimal format ###.## €
+     * @return - a String form of Euro
+     */
     public String toString() {
         return df.format( value() );
     }
 
-    // Euro-Objekte soll man mit equals(Object o) vergleichen können
+    /**
+     * Compares Euro objects to test if they are equal.
+     * Double comparison without delta!
+     * @param o - the reference object with which to compare
+     * @return - true if object is equal
+     */
     public boolean equals(Object o) {
         if ( o instanceof Euro euro) {
             return value() == euro.value();
@@ -38,14 +60,29 @@ public class Euro {
         }
     }
 
+    /**
+     * Get hashcode for the Euro
+     * @return - hashcode of the Euro value
+     */
     public int hashCode() {
         return Double.hashCode( value() );
     }
 
+    /**
+     * Add the value of the Euro to another.
+     * @param euro - the Euro to add
+     * @return a Euro instance with the resulting value
+     */
     public Euro add(Euro euro) {
         return new Euro(value()+euro.value());
     }
 
+    /**
+     * Subtracts the value of the Euro to another
+     * @param euro - the Euro to subtract
+     * @return a Euro instance with the resulting value
+     * @throws IllegalArgumentException - if the result is negativ
+     */
     public Euro subtract(Euro euro) {
         return new Euro(value()-euro.value());
     }
--- src/test/java/de/materna/currency/EuroTest.java
+++ src/test/java/de/materna/currency/EuroTest.java
@@ -3,7 +3,7 @@ package de.materna.currency;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.assertj.core.api.Assertions.*;
 class EuroTest {
 
     @org.junit.jupiter.api.Test
@@ -17,7 +17,8 @@ class EuroTest {
     void subtract() {
         Euro e1 = Euro.of( 3.5 );
         Euro e2 = Euro.of( 2.5 );
-        assertEquals( e1.subtract( e2 ), Euro.of( 1.0 ));
+        assertThat( e1.subtract( e2 ) ).isEqualTo( Euro.of( 1.0 ) );
+        assertThatIllegalArgumentException().isThrownBy( () -> e2.subtract( e1 ) );
     }
 
     @org.junit.jupiter.api.Test
@@ -25,12 +26,22 @@ class EuroTest {
         Euro e1 = Euro.of( 3.5 );
         Euro e3 = Euro.of( 4.0 );
         assertThrows( IllegalArgumentException.class, () -> e1.subtract( e3 ) );
-
         assertThrows( IllegalArgumentException.class, () -> Euro.of( -1.0 ) );
     }
 
     @Test
     void print() {
         assertEquals( "2,35 €", Euro.of( 2.345 ).toString() );
+        assertThat( Euro.of(3.1234) ).isEqualTo( "3,12 €" );
+    }
+
+    @Test
+    void doubleDoof() {
+        Euro e1 = Euro.of( 1.0/3.0 );
+        Euro e2 = Euro.of( 0 );
+        for (int i = 0; i < 300; i++) {
+            e2 = e2.add(e1);
+        }
+        assertThat( e2.value() ).isEqualTo( 10 );
     }
 }
